b632cbf170ce778f1df4fc0397e86b93e463f28d
pavithra.p <41102765+PavithraPalanii@users.noreply.github.com>
2018-07-13 09:41:43 +0530

    Create Alphabet.java

---
+++ Alphabet.java
@@ -0,0 +1,15 @@
+class Alphabet
+{
+	public static void main (String[] args) throws java.lang.Exception
+	{
+		char ch='d';
+		if(ch>=65&&ch<=90 || ch>=97&&ch<=122)
+		{
+			System.out.print("alphabet");
+		}
+		else
+		{
+			System.out.print("not alphabet");
+		}
+	}
+}
