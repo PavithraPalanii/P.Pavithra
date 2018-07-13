d3f2bdf45828556a7f65f1544b1987df53ec6df9
pavithra.p <41102765+PavithraPalanii@users.noreply.github.com>
2018-07-13 09:32:33 +0530

    Create vowel.java

---
+++ vowel.java
@@ -0,0 +1,15 @@
+class vowel
+{
+	public static void main (String[] args) throws java.lang.Exception
+	{
+		char ch='i';
+		if(ch=='a' || ch=='e' || ch=='i' || ch=='o' || ch=='u')
+		{
+			System.out.print(ch +" is vowel");
+		}
+		else
+		{
+			System.out.print(ch +"is consonant");
+		}
+	}
+}
