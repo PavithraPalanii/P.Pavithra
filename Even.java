751988423d5cafc1586ad9426f914cf557230f78
pavithra.p <41102765+PavithraPalanii@users.noreply.github.com>
2018-07-14 15:28:34 +0530

    Create Even.java

---
+++ Even.java
@@ -0,0 +1,17 @@
+import java. util.Scanner;
+public class student {
+public static void main(String[] args)
+{
+	
+			int a=0,i=0,p;
+			String  a1 = "";
+			for(a=6;a<=9;a++)
+			{
+			if(a%2==0)
+			{
+		      System.out.println(a);
+		      
+			}
+			}
+}
+}
