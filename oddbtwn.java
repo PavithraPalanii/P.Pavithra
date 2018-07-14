5309445ad16129adb1285969ede69f63352223d1
pavithra.p <41102765+PavithraPalanii@users.noreply.github.com>
2018-07-14 15:25:18 +0530

    Create oddbtwn.java

---
+++ oddbtwn.java
@@ -0,0 +1,17 @@
+import java. util.Scanner;
+public class student {
+public static void main(String[] args)
+{
+	
+			int a=0,i=0,p;
+			String  a1 = "";
+			for(a=1;a<=6;a++)
+			{
+			if(a%2!=0)
+			{
+		      System.out.println(a);
+		      
+			}
+			}
+}
+}
