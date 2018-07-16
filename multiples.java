cee3ce93164ae53479dc1f606e8f3c00561e315c
pavithra.p <41102765+PavithraPalanii@users.noreply.github.com>
2018-07-16 15:15:33 +0530

    Create multiples.java

---
+++ multiples.java
@@ -0,0 +1,13 @@
+public class Power {
+	public static void main (String [] args) {
+        Scanner s=new Scanner(System.in);
+            int n=s.nextInt();
+            int b=0;
+for(int i=0;i<5;i++)
+{
+	b=b+5;
+System.out.println(b);
+}
+
+	   }
+	}
