2058c4b57fdebad605bab8da95c87ecfa77b42f7
pavithra.p <41102765+PavithraPalanii@users.noreply.github.com>
2018-07-22 10:19:57 +0530

    Create Reverse.java

---
+++ Reverse.java
@@ -0,0 +1,10 @@
+import java.util.Scanner;
+public class Reverse {
+      public static void main(String[] args) {
+    	  Scanner in=new Scanner(System.in);
+    	  String s=in.nextLine();
+	    StringBuffer sb=new StringBuffer(s);
+		System.out.println(sb.reverse());
+		
+  }
+}
