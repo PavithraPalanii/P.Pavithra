8df2bbbc7581a7fbf157fbbb902deac107aa4435
pavithra.p <41102765+PavithraPalanii@users.noreply.github.com>
2018-07-22 11:03:44 +0530

    Create Constraints.java

---
+++ Constraints.java
@@ -0,0 +1,33 @@
+import java.util.Scanner;
+public class Minimum {
+public static void main(String[] args) {
+	int n, temp;
+    Scanner s = new Scanner(System.in);
+    System.out.print("Enter no. of elements you want in array:");
+    n = s.nextInt();
+    int b[] = new int[n];
+    System.out.println("Enter all the elements:");
+    for (int i = 0; i < n; i++) 
+    {
+        b[i] = s.nextInt();
+    }
+    for (int i = 0; i < n; i++) 
+    {
+        for (int j = i + 1; j < n; j++) 
+        {
+            if (b[i] >b[j]) 
+            {
+                temp = b[i];
+                b[i] = b[j];
+                b[j] = temp;
+            }
+        }
+    }
+    System.out.print("ascending Order:");
+    for (int i = 0; i < n-1; i++) 
+    {
+        System.out.print(b[i] + ",");
+    }
+    System.out.print(b[n - 1]);
+}
+}
